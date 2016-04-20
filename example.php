8125ef85bd530243d19a1955aa5ad155a5e15050
Vishwa Datta <v1shwa@yahoo.com>
2016-04-20 15:14:09 -0400

    Applied fixes from StyleCI

--- example.php
+++ example.php
@@ -1,30 +1,28 @@
-<?php 
+<?php
+
 require './src/SimpleGoogleOAuth.php';
 
 // Client Id & Client Secret from Google's developer Console
-$client_id     = 'xxxxx';
+$client_id = 'xxxxx';
 $client_secret = 'xxxxxxxx';
-$redirect_uri  = "http://example.com/example.php";
+$redirect_uri = 'http://example.com/example.php';
 
-
-$client = new SimpleGoogleOAuth($client_id,$client_secret);
+$client = new SimpleGoogleOAuth($client_id, $client_secret);
 
 // We set additional required attributes available at Google developer docs here
 // To provide multiple attributes to a single 'name', seperate them using space, like this
 // $client->setAttr('name', 'value1 value2')
 
 $client->setAttr('redirect_uri', $redirect_uri); // as registered at dev console
-$client->setAttr('scope', "https://www.googleapis.com/auth/drive.readonly https://www.googleapis.com/auth/drive.apps.readonly");
+$client->setAttr('scope', 'https://www.googleapis.com/auth/drive.readonly https://www.googleapis.com/auth/drive.apps.readonly');
 $client->setAttr('access_type', 'offline'); // and so on
 
-
-if (!isset($_GET['code']))
-{
+if (!isset($_GET['code'])) {
     // Create auth url
     $auth_url = $client->createAuthUrl();
-    
+
     // For web apps, we redirect user here
-    header('Location: ' . $auth_url);
+    header('Location: '.$auth_url);
     die("<a href='$auth_url'>Click here.</a>");
 } else {
 
@@ -34,8 +32,7 @@ if (!isset($_GET['code']))
     // To save this as JSON, call  getAccessToken()
     $json_access_token = $client->getAccessToken();
     // and save to file or database
-    file_put_contents("user_access_token.secret", $json_access_token);
-
+    file_put_contents('user_access_token.secret', $json_access_token);
 
     // Output
     echo $json_access_token;
@@ -45,17 +42,13 @@ if (!isset($_GET['code']))
     // If you want to refresh this saved access token after expiration
     // You could either pass JSON string or Array to setAccessToken() method, like this
     // Note: Token Can be refreshed after expiry only if you set access_type to offline.
-    
-    $tok = file_get_contents("user_access_token.secret");
+
+    $tok = file_get_contents('user_access_token.secret');
     $client->setAccessToken($tok);
 
     //Checkk if token expired
-    if ( $client->isAccessTokenExpired() ) {
+    if ($client->isAccessTokenExpired()) {
         //  generate new token
-        $access_token = $client->refreshToken();// throws an Exception, if refresh_token is not available
+        $access_token = $client->refreshToken(); // throws an Exception, if refresh_token is not available
     }
-
 }
-
-
-?>
--- src/SimpleGoogleOauth.php
+++ src/SimpleGoogleOauth.php
@@ -1,4 +1,5 @@
-<?php 
+<?php
+
 /*
  * Copyright 2015 Vishwa Datta
  *
@@ -16,273 +17,288 @@
  */
 
 /**
- * Simple Google OAuth Library
- * 
+ * Simple Google OAuth Library.
+ *
  * @version     1.0-dev
+ *
  * @author      Vishwa <vishwadatta05@gmail.com>
  */
 
-
 /**
-* Google Auth
-*/
-class SimpleGoogleOAuth
+ * Google Auth.
+ */
+class SimpleGoogleOauth
 {
-	/**
-	 * Authorization URL
-	 * @var string
-	 */		
-	protected $auth_endpoint  = 'https://accounts.google.com/o/oauth2/auth';
-	
+    /**
+     * Authorization URL.
+     *
+     * @var string
+     */
+    protected $auth_endpoint = 'https://accounts.google.com/o/oauth2/auth';
 
-	/**
-	 * Access Token URl
-	 * @var string
-	 */
-	protected $token_endpoint = 'https://accounts.google.com/o/oauth2/token';
+    /**
+     * Access Token URl.
+     *
+     * @var string
+     */
+    protected $token_endpoint = 'https://accounts.google.com/o/oauth2/token';
 
+    /**
+     * Access Token Data.
+     *
+     * @var array
+     */
+    protected $token_data = [];
 
-	/**
-	 * Access Token Data
-	 * @var array
-	 */
-	protected $token_data = array();
+    /**
+     * Client Id.
+     *
+     * @var string
+     */
+    protected $client_id = null;
 
+    /**
+     * Client Secret.
+     *
+     * @var string
+     */
+    protected $client_secret = null;
 
-	/**
-	 * Client Id
-	 * @var string
-	 */
-	protected $client_id = null;
+    /**
+     * Configuration file (adapted from google's official PHP client).
+     *
+     * @var array
+     */
+    private $configuration = [
+            'appname'                    => 'My Google App',
+            'response_type'              => 'code',
+            'redirect_uri'               => '',
+            'scope'                      => '',
 
+            //Server key
+            'developer_key'              => '',
 
-	/**
-	 * Client Secret
-	 * @var string
-	 */
-	protected $client_secret = null;
+            // Other parameters.
+            'hd'                         => '',
+            'prompt'                     => '',
+            'openid.realm'               => '',
+            'include_granted_scopes'     => '',
+            'login_hint'                 => '',
+            'request_visible_actions'    => '',
+            'access_type'                => 'online',
+            'approval_prompt'            => 'auto',
+            'federated_signon_certs_url' => 'https://www.googleapis.com/oauth2/v1/certs',
+      ];
 
+    /**
+     * Constructing the GoogleOAuth Library.
+     *
+     * @param string $client_id     Client Id (required)
+     * @param string $client_secret Client Secret (required)
+     */
+    public function __construct($client_id = null, $client_secret = null)
+    {
+        if (!($client_id and $client_secret)) {
+            throw new Exception('client_id & client_secret are mandatory.');
+        }
 
+        $this->client_id = $client_id;
+        $this->client_secret = $client_secret;
+    }
 
-	/**
-	 * Configuration file (adapted from google's official PHP client).
-	 * @var array
-	 */
-	private $configuration = array(
-			'appname'                    => 'My Google App',
-			'response_type'              => 'code',
-			'redirect_uri'               => '',
-			'scope'                      => '',
-			
-			//Server key
-			'developer_key'              => '',
-			
-			// Other parameters.
-			'hd'                         => '',
-			'prompt'                     => '',
-			'openid.realm'               => '',
-			'include_granted_scopes'     => '',
-			'login_hint'                 => '',
-			'request_visible_actions'    => '',
-			'access_type'                => 'online',
-			'approval_prompt'            => 'auto',
-			'federated_signon_certs_url' => 'https://www.googleapis.com/oauth2/v1/certs'
-      );
+    /**
+     * Set different attributes in the $configuration error.
+     *
+     * @param string $key   name
+     * @param string $value value
+     */
+    public function setAttr($key, $value)
+    {
+        if (array_key_exists($key, $this->configuration)) {
+            $this->configuration[$key] = $value;
+        } else {
+            throw new Exception("Invalid Configuration Key: $key");
+        }
+    }
 
+    /**
+     * Get the $configuration values.
+     *
+     * @param string $key name
+     *
+     * @return string value
+     */
+    public function getAttr($key)
+    {
+        if (array_key_exists($key, $this->configuration)) {
+            return $this->configuration[$key];
+        } else {
+            throw new Exception("Configuration Key $key doesn't exist");
+        }
+    }
 
-	/**
-	 * Constructing the GoogleOAuth Library.
-	 * @param string $client_id     Client Id (required)
-	 * @param string $client_secret Client Secret (required)
-	 */
-	function __construct($client_id=null, $client_secret=null) 
-	{
-		if ( !($client_id AND $client_secret) ) {
-			throw new Exception("client_id & client_secret are mandatory.");
-		}
+    /**
+     * Build the Authentication URL.
+     *
+     * @return string URL
+     */
+    public function createAuthUrl()
+    {
+        $params = ['client_id' => $this->client_id];
+        foreach ($this->configuration as $k => $v) {
+            if ($v != '') {
+                $params[$k] = $v;
+            }
+        }
 
-		$this->client_id = $client_id;
-		$this->client_secret = $client_secret;
-	}
+        return $this->auth_endpoint.'?'.http_build_query($params, '', '&');
+    }
 
+    /**
+     * Set the access Token.
+     *
+     * @param mixed $tok access tokens (could be Array or JSON)
+     */
+    public function setAccessToken($tok)
+    {
 
-	/**
-	 * Set different attributes in the $configuration error.
-	 * @param string $key   name
-	 * @param string $value value
-	 */
-	public function setAttr($key, $value) 
-	{
-		if ( array_key_exists($key, $this->configuration) )
-			 $this->configuration[$key] = $value;
-		else 
-			throw new Exception("Invalid Configuration Key: $key");
-	}
+        // If not array, this should be JSON.
+        if (!is_array($tok)) {
+            $tok = json_decode($tok, true);
+        }
 
+        if ($tok == null) {
+            throw new Exception('Invalid token format. Expecting be array or json string');
+        }
 
-	/**
-	 * Get the $configuration values.
-	 * @param  string $key name
-	 * @return string      value
-	 */
-	public function getAttr($key)
-	{
-		if (array_key_exists($key, $this->configuration))
-			return $this->configuration[$key];
-		else 
-			throw new Exception("Configuration Key $key doesn't exist");
-	}
+        if (!isset($tok['access_token'])) {
+            throw new Exception('Token must contain access_token key');
+        }
 
+        $this->token_data = $tok;
+    }
 
-	/**
-	 * Build the Authentication URL.
-	 * @return string URL
-	 */
-	public function createAuthUrl()
-	{
-		$params = array('client_id' => $this->client_id);
-		foreach ($this->configuration as $k => $v) {
-			if ($v != '') $params[$k] = $v;
-		}
+    /**
+     * Create Access Token from Code.
+     *
+     * @param string $code Code value
+     *
+     * @return array accesstoken
+     */
+    public function createAccessToken($code)
+    {
+        if (empty($code)) {
+            throw new Exception('code cannot be empty.');
+        }
 
-		return $this->auth_endpoint .'?'. http_build_query($params, '', '&');
-	}
-
-
-	/**
-	 * Set the access Token.
-	 * @param mixed $tok access tokens (could be Array or JSON)
-	 */
-	public function setAccessToken($tok) {
-
-		// If not array, this should be JSON.
-		if (!is_array($tok)) $tok = json_decode($tok, true);
-		
-	    if ($tok == null) 
-	      throw new Exception("Invalid token format. Expecting be array or json string");
-	    
-	    if ( !isset($tok['access_token']) ) 
-	      throw new Exception("Token must contain access_token key");
-	    
-	    $this->token_data = $tok;
-	}
-
-
-	/**
-	 * Create Access Token from Code.
-	 * @param  string $code Code value
-	 * @return array       accesstoken
-	 */
-	public function createAccessToken($code)
-	{
-		if (empty($code)) {
-			throw new Exception("code cannot be empty.");
-		}
-
-
-		$params = array(
-          'code' => $code,
-          'grant_type' => 'authorization_code',
-          'client_id' =>  $this->client_id,
+        $params = [
+          'code'          => $code,
+          'grant_type'    => 'authorization_code',
+          'client_id'     => $this->client_id,
           'client_secret' => $this->client_secret,
-          'redirect_uri' => $this->configuration['redirect_uri']
-    	);
+          'redirect_uri'  => $this->configuration['redirect_uri'],
+        ];
 
-		$this->token_data    = $this->makeRequest($this->token_endpoint , $params);
-		return $this->token_data;
-	}
+        $this->token_data = $this->makeRequest($this->token_endpoint, $params);
 
-	/**
-	 * Get Access Token in JSON to store in database or files.
-	 * @return json access token data
-	 */
-	public function getAccessToken()
-	{
-		return json_encode($this->token_data);
-	}
+        return $this->token_data;
+    }
 
-	/**
-	 * Check if Access Token has expired.
-	 * @return boolean true if expired
-	 */
-	public function isAccessTokenExpired() 
-	{
-		if (isset($this->token_data['created_at'])) {
-			$expired = ( $this->token_data['created_at']+ ($this->token_data['expires_in'] - 30) ) < time();
-		} else {
-			$query   = @file_get_contents("https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=".$this->token_data['access_token']);
-			$expired = ( json_decode($query,true)['expires_in'] ) ? false : true;
-		}
-		return $expired;
-	}
+    /**
+     * Get Access Token in JSON to store in database or files.
+     *
+     * @return json access token data
+     */
+    public function getAccessToken()
+    {
+        return json_encode($this->token_data);
+    }
 
+    /**
+     * Check if Access Token has expired.
+     *
+     * @return bool true if expired
+     */
+    public function isAccessTokenExpired()
+    {
+        if (isset($this->token_data['created_at'])) {
+            $expired = ($this->token_data['created_at'] + ($this->token_data['expires_in'] - 30)) < time();
+        } else {
+            $query = @file_get_contents('https://www.googleapis.com/oauth2/v1/tokeninfo?access_token='.$this->token_data['access_token']);
+            $expired = (json_decode($query, true)['expires_in']) ? false : true;
+        }
 
-	/**
-	 * Creates a new access Token.
-	 * @return array refreshed token
-	 */
-	public function refreshToken()
-	{
-		if (! array_key_exists('refresh_token', $this->token_data) )
-			throw new Exception("refresh_token doesn't exist");
-			
+        return $expired;
+    }
 
-	    $params =  array(
-          'client_id' => $this->client_id,
+    /**
+     * Creates a new access Token.
+     *
+     * @return array refreshed token
+     */
+    public function refreshToken()
+    {
+        if (!array_key_exists('refresh_token', $this->token_data)) {
+            throw new Exception("refresh_token doesn't exist");
+        }
+
+        $params = [
+          'client_id'     => $this->client_id,
           'client_secret' => $this->client_secret,
           'refresh_token' => $this->token_data['refresh_token'],
-          'grant_type' => 'refresh_token'
-        );
+          'grant_type'    => 'refresh_token',
+        ];
 
-	    $this->token_data    = $this->makeRequest($this->token_endpoint , $params);
-		return $this->token_data;
-	}
+        $this->token_data = $this->makeRequest($this->token_endpoint, $params);
 
+        return $this->token_data;
+    }
 
-	/**
-	 * Perform Curl Operation.
-	 * @param  string $url    Url
-	 * @param  array $params arguments to POST
-	 * @return array         result
-	 */
-	private function makeRequest($url, $params=array() )
-	{
-		$ch = curl_init();
+    /**
+     * Perform Curl Operation.
+     *
+     * @param string $url    Url
+     * @param array  $params arguments to POST
+     *
+     * @return array result
+     */
+    private function makeRequest($url, $params = [])
+    {
+        $ch = curl_init();
 
-		curl_setopt($ch, CURLOPT_USERAGENT, 'User-Agent: '. $this->configuration['appname']);
-		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-		curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
-		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-		curl_setopt($ch,CURLOPT_URL, $url);
-		curl_setopt($ch,CURLOPT_POST, count($params));
-		curl_setopt($ch,CURLOPT_POSTFIELDS,  http_build_query($params, null, '&') );
+        curl_setopt($ch, CURLOPT_USERAGENT, 'User-Agent: '.$this->configuration['appname']);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_POST, count($params));
+        curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($params, null, '&'));
 
-		$result        = curl_exec($ch);
+        $result = curl_exec($ch);
 
-		$response_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+        $response_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
 
-		if ( $curl_error = curl_error($ch) )
+        if ($curl_error = curl_error($ch)) {
             throw new Exception("Curl Request Failed: $curl_error");
+        }
 
-		curl_close($ch);
+        curl_close($ch);
 
         $result = json_decode($result, true);
 
         // Check for valid http code.
-        if ( 200 != $response_code) 
-        	throw new Exception("Error:". $result['error'] . '-'.$result['error_description']);
+        if (200 != $response_code) {
+            throw new Exception('Error:'.$result['error'].'-'.$result['error_description']);
+        }
 
         // Check if error code exists.
-		if (@$result['code']){
-			$err_msg = "Error: ". $result['result']['error'] . ' - '.  $result['result']['error_description'];
-			throw new Exception($err_msg);
-		}
-		
-		$result['created_at'] = time();
-	    return $result;
-	}
+        if (@$result['code']) {
+            $err_msg = 'Error: '.$result['result']['error'].' - '.$result['result']['error_description'];
+            throw new Exception($err_msg);
+        }
 
+        $result['created_at'] = time();
 
+        return $result;
+    }
 }
-
-?>
